907491ccf2c2cfa01a2f7d31af65222808a494d2
PandaHero <675051285@qq.com>
2017-12-08 11:23:34 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	taobaofood/test/test_1.py

---
+++ jiuxian_wine/grape_wine.py
@@ -0,0 +1,5 @@
+import urllib.request
+from urllib.request import  urlopen
+
+t = urllib.request.unquote("http%3a%2f%2fitem.jd.com%2f13645528662.html")
+print(t)
--- test/test_2.py
+++
@@ -1,4 +0,0 @@
-import csv
-with open(r"C:\Users\chen\Desktop\欧盟微博关键字.csv","r+") as file:
-    for line in csv.reader(file):
-        print(line)
